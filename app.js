6a1ad588bc8c3eee6401ff422b77f78e6e06b756
shafee_azeem <shafeeazeemag@gmail.com>
2024-01-28 05:45:32 +0530

    construtors

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+class ToyotaCar{
 
+    constructor(brand,mileage){
+        console.log("creating a new Object"); 
+        this.brand=brand; 
+    }
+
+    start(){
+        console.log("Starts the car"); 
+    }
+    stop(){
+        console.log("Stops the car"); 
+    }
+    setBrand(brand){
+        this.brand= brand;
+    }
+}
+
+let fortuner= new ToyotaCar("fortuner",14KMPL); 
+let lexus= new ToyotaCar("Lexus",12KMPL);
+ 
